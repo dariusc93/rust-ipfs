b884bb88650bfdf2c54051c0ee3de02d173af2f7
Darius Clark <darius.clark93@gmail.com>
2022-12-07 09:43:18 -0500

    refactor: Filter out existing providers from new providers
    Since we are returning a stream with the providers, it would probably be best to filter out the providers from existing ones provided here so the stream would only output ones that havent been provided there.

--- src/p2p/provider_stream.rs
+++ src/p2p/provider_stream.rs
@@ -11,11 +11,13 @@ use libp2p::PeerId;
 pub struct ProviderStream {
     finished: bool,
     rx: UnboundedReceiver<HashSet<PeerId>>,
+    providers: HashSet<PeerId>,
 }
 
 impl ProviderStream {
     pub fn new(rx: UnboundedReceiver<HashSet<PeerId>>) -> Self {
         Self {
+            providers: Default::default(),
             finished: false,
             rx,
         }
@@ -27,11 +29,20 @@ impl Stream for ProviderStream {
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
         match Pin::new(&mut self.rx).poll_next_unpin(cx) {
+            Poll::Ready(Some(providers)) => {
+                let providers = providers
+                    .difference(&self.providers)
+                    .copied()
+                    .collect::<HashSet<_>>();
+
+                self.providers.extend(providers.clone());
+                Poll::Ready(Some(providers))
+            }
             Poll::Ready(None) => {
                 self.finished = true;
                 Poll::Ready(None)
             }
-            next => next,
+            Poll::Pending => Poll::Pending,
         }
     }
 }
