2e5bedbc91d5a585f4a484526ddc394f582410b9
Darius Clark <darius.clark93@gmail.com>
2022-12-22 11:44:48 -0500

    chore: Reexport libipld block outside of bitswap

--- src/lib.rs
+++ src/lib.rs
@@ -97,7 +97,9 @@ pub use self::{
     path::IpfsPath,
     repo::{PinKind, PinMode, RepoTypes},
 };
-pub use ipfs_bitswap::Block;
+
+pub type Block = libipld::Block<libipld::DefaultParams>;
+
 use libipld::{
     multibase::{self, Base},
     Cid, Ipld, IpldCodec,
