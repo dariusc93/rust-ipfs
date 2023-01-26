f3cd19c3a02e05a08e886d1c76a3e69bad5fc121
Darius Clark <darius.clark93@gmail.com>
2023-01-26 18:39:58 -0500

    chore: Update local-node example
    - Provide command line option for bootstrapping and enable port mapping
    
    Note: Related to commit #9

--- examples/local-node.rs
+++ examples/local-node.rs
@@ -1,26 +1,63 @@
-use rust_ipfs::{Ipfs, IpfsOptions, TestTypes, UninitializedIpfs, PublicKey, p2p::PeerInfo};
+use clap::Parser;
+use rust_ipfs::{p2p::PeerInfo, Ipfs, IpfsOptions, PublicKey, TestTypes, UninitializedIpfs};
+use tokio::sync::Notify;
+
+#[derive(Debug, Parser)]
+#[clap(name = "local-node")]
+struct Opt {
+    #[clap(long)]
+    portmapping: bool,
+    #[clap(long)]
+    bootstrap: bool,
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let opt = Opt::parse();
     tracing_subscriber::fmt::init();
 
     // Initialize the repo and start a daemon
-    let opts = IpfsOptions::default();
+    let opts = IpfsOptions {
+        port_mapping: opt.portmapping,
+        ..Default::default()
+    };
+
     let ipfs: Ipfs<TestTypes> = UninitializedIpfs::new(opts).spawn_start().await?;
-    
+
+    if opt.bootstrap {
+        ipfs.default_bootstrap().await?;
+        tokio::spawn({
+            let ipfs = ipfs.clone();
+            async move {
+                let _ = ipfs.bootstrap().await?;
+                Ok::<_, anyhow::Error>(())
+            }
+        });
+    }
+
     tokio::time::sleep(std::time::Duration::from_secs(1)).await;
 
-    let PeerInfo { public_key: key, listen_addrs: addresses, ..} = ipfs.identity(None).await?;
-    
+    let PeerInfo {
+        public_key: key,
+        listen_addrs: addresses,
+        ..
+    } = ipfs.identity(None).await?;
+
     if let PublicKey::Ed25519(publickey) = &key {
-        println!("Public Key: {}", bs58::encode(publickey.encode()).into_string());
+        println!(
+            "Public Key: {}",
+            bs58::encode(publickey.encode()).into_string()
+        );
     }
-    
+
     println!("PeerID: {}", key.to_peer_id());
 
     for address in addresses {
         println!("Listening on: {}", address);
     }
+
+    // Used to wait until the process is terminated instead of creating a loop
+    Notify::new().notified().await;
 
     ipfs.exit_daemon().await;
     Ok(())
