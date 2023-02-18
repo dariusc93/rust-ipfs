1393db48020fd70468c04a051bae4ab334053ebc
Darius Clark <darius.clark93@gmail.com>
2023-02-17 21:13:35 -0500

    chore: Remove async from event handling functions

--- src/task.rs
+++ src/task.rs
@@ -92,7 +92,7 @@ impl<TRepoTypes: RepoTypes> IpfsTask<TRepoTypes> {
             tokio::select! {
                 swarm = self.swarm.next() => {
                     if let Some(swarm) = swarm {
-                        self.handle_swarm_event(swarm).await;
+                        self.handle_swarm_event(swarm);
                     }
                 },
                 event = self.from_facade.next() => {
@@ -100,12 +100,12 @@ impl<TRepoTypes: RepoTypes> IpfsTask<TRepoTypes> {
                         if matches!(event, IpfsEvent::Exit) {
                             break;
                         }
-                        self.handle_event(event).await;
+                        self.handle_event(event);
                     }
                 },
                 repo = self.repo_events.next() => {
                     if let Some(repo) = repo {
-                        self.handle_repo_event(repo).await;
+                        self.handle_repo_event(repo);
                     }
                 }
             }
@@ -116,7 +116,7 @@ impl<TRepoTypes: RepoTypes> IpfsTask<TRepoTypes> {
         }
     }
 
-    async fn handle_swarm_event(&mut self, swarm_event: TSwarmEvent) {
+    fn handle_swarm_event(&mut self, swarm_event: TSwarmEvent) {
         if let Some(handler) = self.swarm_event.clone() {
             handler(&mut self.swarm, &swarm_event)
         }
@@ -583,7 +583,7 @@ impl<TRepoTypes: RepoTypes> IpfsTask<TRepoTypes> {
         }
     }
 
-    async fn handle_event(&mut self, event: IpfsEvent) {
+    fn handle_event(&mut self, event: IpfsEvent) {
         match event {
             IpfsEvent::Connect(target, ret) => {
                 ret.send(self.swarm.behaviour_mut().connect(target)).ok();
@@ -956,7 +956,8 @@ impl<TRepoTypes: RepoTypes> IpfsTask<TRepoTypes> {
             }
         }
     }
-    async fn handle_repo_event(&mut self, event: RepoEvent) {
+
+    fn handle_repo_event(&mut self, event: RepoEvent) {
         match event {
             RepoEvent::WantBlock(cid) => self.swarm.behaviour_mut().want_block(cid),
             RepoEvent::UnwantBlock(cid) => self.swarm.behaviour_mut().bitswap().cancel_block(&cid),
