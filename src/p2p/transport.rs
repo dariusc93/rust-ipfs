96b1fc4e4c782191289a1a252a4dc703f2cf5c38
Darius Clark <darius.clark93@gmail.com>
2023-02-25 08:25:20 -0500

    refactor/transport: Refactored TransportConfig and added customizable fields
    - Added DnsResolver to pick between which resolver to use
    - Added UpgradeVersion to pick between Version::V1 and Version::V1Lazy (Note: This might get removed in the future)
    - Remove YamuxConfig and MplexConfig and added fields to map value to the corresponding functions

--- src/p2p/transport.rs
+++ src/p2p/transport.rs
@@ -4,7 +4,7 @@ use libp2p::core::transport::timeout::TransportTimeout;
 use libp2p::core::transport::upgrade::Version;
 use libp2p::core::transport::{Boxed, OrTransport};
 use libp2p::core::upgrade::SelectUpgrade;
-use libp2p::dns::{ResolverConfig, TokioDnsConfig};
+use libp2p::dns::{ResolverConfig, ResolverOpts, TokioDnsConfig};
 use libp2p::identity;
 use libp2p::mplex::MplexConfig;
 use libp2p::noise::{self, NoiseConfig};
@@ -16,37 +16,79 @@ use libp2p::yamux::{WindowUpdateMode, YamuxConfig};
 use libp2p::{PeerId, Transport};
 use std::io;
 use std::time::Duration;
+use trust_dns_resolver::system_conf;
 
 /// Transport type.
 pub(crate) type TTransport = Boxed<(PeerId, StreamMuxerBox)>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 pub struct TransportConfig {
-    pub yamux_config: YamuxConfig,
-    pub mplex_config: MplexConfig,
+    pub yamux_max_buffer_size: usize,
+    pub yamux_receive_window_size: u32,
+    pub yamux_update_mode: u8,
+    pub mplex_max_buffer_size: usize,
     pub no_delay: bool,
     pub port_reuse: bool,
     pub timeout: Duration,
+    pub dns_resolver: Option<DnsResolver>,
+    pub version: Option<UpgradeVersion>,
 }
 
 impl Default for TransportConfig {
     fn default() -> Self {
         Self {
-            yamux_config: {
-                let mut config = YamuxConfig::default();
-                config.set_max_buffer_size(16 * 1024 * 1024);
-                config.set_receive_window_size(16 * 1024 * 1024);
-                config.set_window_update_mode(WindowUpdateMode::on_receive());
-                config
-            },
-            mplex_config: {
-                let mut config = MplexConfig::default();
-                config.set_max_buffer_size(usize::MAX);
-                config
-            },
+            yamux_max_buffer_size: 8 * 1024 * 1024,
+            yamux_receive_window_size: 8 * 1024 * 1024,
+            yamux_update_mode: 0,
+            mplex_max_buffer_size: 1024,
             no_delay: true,
             port_reuse: true,
             timeout: Duration::from_secs(30),
+            dns_resolver: None,
+            version: None,
+        }
+    }
+}
+
+#[derive(Default, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum DnsResolver {
+    /// Google DNS Resolver
+    Google,
+    /// Cloudflare DNS Resolver
+    #[default]
+    Cloudflare,
+    /// Local DNS Resolver
+    Local,
+    /// No DNS Resolver
+    None,
+}
+
+impl From<DnsResolver> for (ResolverConfig, ResolverOpts) {
+    fn from(value: DnsResolver) -> Self {
+        match value {
+            DnsResolver::Google => (ResolverConfig::google(), Default::default()),
+            DnsResolver::Cloudflare => (ResolverConfig::cloudflare(), Default::default()),
+            DnsResolver::Local => system_conf::read_system_conf().unwrap_or_default(),
+            DnsResolver::None => (ResolverConfig::new(), Default::default()),
+        }
+    }
+}
+
+#[derive(Default, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum UpgradeVersion {
+    /// See [`Version::V1`]
+    #[default]
+    Standard,
+
+    /// See [`Version::V1Lazy`]
+    Lazy,
+}
+
+impl From<UpgradeVersion> for Version {
+    fn from(value: UpgradeVersion) -> Self {
+        match value {
+            UpgradeVersion::Standard => Version::V1,
+            UpgradeVersion::Lazy => Version::V1Lazy,
         }
     }
 }
@@ -57,21 +99,41 @@ impl Default for TransportConfig {
 pub fn build_transport(
     keypair: identity::Keypair,
     relay: Option<ClientTransport>,
-    config: TransportConfig,
-) -> io::Result<TTransport> {
-    let TransportConfig {
-        yamux_config,
-        mplex_config,
+    TransportConfig {
         no_delay,
         port_reuse,
         timeout,
-    } = config;
+        yamux_max_buffer_size,
+        yamux_receive_window_size,
+        yamux_update_mode,
+        mplex_max_buffer_size,
+        dns_resolver,
+        version,
+    }: TransportConfig,
+) -> io::Result<TTransport> {
     let xx_keypair = noise::Keypair::<noise::X25519Spec>::new()
         .into_authentic(&keypair)
         .unwrap();
     let noise_config = NoiseConfig::xx(xx_keypair).into_authenticated();
 
-    let multiplex_upgrade = SelectUpgrade::new(yamux_config, mplex_config);
+    let multiplex_upgrade = SelectUpgrade::new(
+        {
+            let mut config = YamuxConfig::default();
+            config.set_max_buffer_size(yamux_max_buffer_size);
+            config.set_receive_window_size(yamux_receive_window_size);
+            config.set_window_update_mode(if yamux_update_mode == 0 {
+                WindowUpdateMode::on_receive()
+            } else {
+                WindowUpdateMode::on_read()
+            });
+            config
+        },
+        {
+            let mut config = MplexConfig::default();
+            config.set_max_buffer_size(mplex_max_buffer_size);
+            config
+        },
+    );
 
     //TODO: Cleanup
     let tcp_config = GenTcpConfig::default()
@@ -88,19 +150,18 @@ pub fn build_transport(
 
     let transport_timeout = TransportTimeout::new(transport, Duration::from_secs(30));
 
-    //TODO: Make this configurable to use, google, cloudflare or a custom resolver
-    let transport = TokioDnsConfig::custom(
-        transport_timeout,
-        ResolverConfig::cloudflare(),
-        Default::default(),
-    )?;
+    let (cfg, opts) = dns_resolver.unwrap_or_default().into();
+
+    let transport = TokioDnsConfig::custom(transport_timeout, cfg, opts)?;
+
+    let version = version.unwrap_or_default();
 
     let transport = match relay {
         Some(relay) => {
             let transport = OrTransport::new(relay, transport);
 
             transport
-                .upgrade(Version::V1)
+                .upgrade(version.into())
                 .authenticate(noise_config)
                 .multiplex(multiplex_upgrade)
                 .timeout(timeout)
@@ -108,7 +169,7 @@ pub fn build_transport(
                 .boxed()
         }
         None => transport
-            .upgrade(Version::V1)
+            .upgrade(version.into())
             .authenticate(noise_config)
             .multiplex(multiplex_upgrade)
             .timeout(timeout)
