4e4777760d9a02bdf9e05966363fce558c7dee63
Darius Clark <darius.clark@proton.me>
2023-06-03 01:21:06 -0400

    feat: Implemented MultiaddrExt trait

--- src/p2p/addr.rs
+++ src/p2p/addr.rs
@@ -9,6 +9,78 @@ use std::{
     str::FromStr,
 };
 
+pub trait MultiaddrExt {
+    /// Peer id
+    fn peer_id(&self) -> Option<PeerId>;
+    /// Relay peer id
+    fn relay_peer_id(&self) -> Option<PeerId>;
+
+    /// Address that only doesnt include peer protocols
+    fn address(&self) -> Option<Multiaddr>;
+
+    /// Determine if the address is a relay circuit
+    fn is_relay(&self) -> bool;
+
+    /// Determine if the address is being relayed to a peer
+    fn is_relayed(&self) -> bool;
+}
+
+impl MultiaddrExt for Multiaddr {
+    fn peer_id(&self) -> Option<PeerId> {
+        if let Some(Protocol::P2p(hash)) = self.iter().last() {
+            return PeerId::from_multihash(hash).ok();
+        }
+
+        None
+    }
+
+    fn relay_peer_id(&self) -> Option<PeerId> {
+        if !self.is_relay() {
+            return None;
+        }
+
+        while let Some(protocol) = self.iter().next() {
+            //Find the first peer id and return it
+            if let Protocol::P2p(hash) = protocol {
+                return PeerId::from_multihash(hash).ok();
+            }
+        }
+
+        None
+    }
+
+    fn address(&self) -> Option<Multiaddr> {
+        let mut addr = self.clone();
+        while let Some(proto) = addr.pop() {
+            if matches!(
+                proto,
+                Protocol::Ip4(_) | Protocol::Ip6(_) | Protocol::Dnsaddr(_)
+            ) {
+                return Some(addr);
+            }
+        }
+
+        None
+    }
+
+    fn is_relay(&self) -> bool {
+        self.iter()
+            .any(|proto| matches!(proto, Protocol::P2pCircuit))
+    }
+
+    fn is_relayed(&self) -> bool {
+        if !self.is_relay() {
+            return false;
+        }
+
+        if self.peer_id().is_none() {
+            return false;
+        }
+
+        true
+    }
+}
+
 /// An error that can be thrown when converting to `MultiaddrWithPeerId` and
 /// `MultiaddrWithoutPeerId`.
 #[derive(Debug)]
@@ -207,18 +279,21 @@ mod tests {
 
     #[test]
     fn connection_targets() {
-        let peer_id = "QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ";
-        let multiaddr_wo_peer = "/ip4/104.131.131.82/tcp/4001";
-        let multiaddr_with_peer = format!("{multiaddr_wo_peer}/p2p/{peer_id}");
-        let p2p_peer = format!("/p2p/{peer_id}");
-        // note: /ipfs/peer_id doesn't properly parse as a Multiaddr
-        let mwp = multiaddr_with_peer.parse::<MultiaddrWithPeerId>().unwrap();
+        let peer_id = PeerId::from_str("QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ")
+            .expect("Valid peer id");
+        let multiaddr_wo_peer =
+            Multiaddr::from_str("/ip4/104.131.131.82/tcp/4001").expect("Valid multiaddr");
+        let multiaddr_with_peer =
+            Multiaddr::from_str(&format!("{multiaddr_wo_peer}/p2p/{peer_id}"))
+                .expect("valid multiaddr");
+        let p2p_peer = Multiaddr::from_str(&format!("/p2p/{peer_id}")).expect("Valid multiaddr");
 
-        assert!(multiaddr_wo_peer.parse::<MultiaddrWithoutPeerId>().is_ok());
-        assert_eq!(
-            Multiaddr::from(mwp),
-            multiaddr_with_peer.parse::<Multiaddr>().unwrap()
-        );
-        assert!(p2p_peer.parse::<Multiaddr>().is_ok());
+        assert!(multiaddr_wo_peer.peer_id().is_none());
+        assert!(multiaddr_with_peer.peer_id().is_some());
+        assert!(p2p_peer.peer_id().is_some());
+
+        let peer_id_target = multiaddr_with_peer.peer_id().expect("Valid peer id");
+
+        assert_eq!(peer_id_target, peer_id);
     }
 }
