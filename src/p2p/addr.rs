32c663bc9f38ed8e20eaae6552c3acf81c35ff65
Darius Clark <darius.clark@proton.me>
2023-06-12 09:40:24 -0400

    fix: dont use old addr

--- src/p2p/addr.rs
+++ src/p2p/addr.rs
@@ -99,7 +99,7 @@ pub(crate) fn peer_id_from_multiaddr(addr: Multiaddr) -> Option<PeerId> {
 pub(crate) fn extract_peer_id_from_multiaddr(mut addr: Multiaddr) -> (Option<PeerId>, Multiaddr) {
     let old_addr = addr.clone();
     match addr.pop() {
-        Some(Protocol::P2p(peer)) => (Some(peer), old_addr),
+        Some(Protocol::P2p(peer)) => (Some(peer), addr),
         _ => (None, old_addr),
     }
 }
