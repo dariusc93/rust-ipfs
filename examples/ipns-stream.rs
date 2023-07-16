978f356f93c45265810b79a3f6964126b51627cb
Darius Clark <darius.clark@proton.me>
2023-07-16 02:55:21 -0400

    chore: Added example

---
+++ examples/ipns-stream.rs
@@ -0,0 +1,32 @@
+use futures::StreamExt;
+use libipld::multibase::{self, Base};
+use rust_ipfs::Ipfs;
+use rust_ipfs::UninitializedIpfsNoop as UninitializedIpfs;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    // Initialize the repo and start a daemon
+    let ipfs: Ipfs = UninitializedIpfs::new()
+        .enable_mdns()
+        .default_record_key_validator()
+        .start()
+        .await?;
+
+    ipfs.default_bootstrap().await?;
+
+    let mut stream = ipfs
+        .dht_get("<ipns key here>")
+        .await?;
+
+    while let Some(record) = stream.next().await {
+        //TODO: Look into a better conversion
+        let key = &record.key.as_ref()[6..];
+        let encoded_key = &multibase::encode(Base::Base58Btc, key)[1..];
+        println!("Record Key: {encoded_key}");
+        println!();
+    }
+
+    Ok(())
+}
