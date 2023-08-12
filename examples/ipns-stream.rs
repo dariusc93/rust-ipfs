a8a06a4599c56fdb20e63eb11e2d54eff8ac9416
Darius Clark <darius.clark@proton.me>
2023-08-11 22:18:36 -0400

    chore: Update example

--- examples/ipns-stream.rs
+++ examples/ipns-stream.rs
@@ -1,11 +1,20 @@
+use clap::Parser;
 use futures::StreamExt;
 use libipld::multibase::{self, Base};
 use rust_ipfs::Ipfs;
 use rust_ipfs::UninitializedIpfsNoop as UninitializedIpfs;
 
+#[derive(Debug, clap::Parser)]
+#[clap(name = "ipns")]
+struct Opt {
+    key: String,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
+
+    let opt = Opt::parse();
 
     // Initialize the repo and start a daemon
     let ipfs: Ipfs = UninitializedIpfs::new()
@@ -16,15 +25,15 @@ async fn main() -> anyhow::Result<()> {
 
     ipfs.default_bootstrap().await?;
 
-    let mut stream = ipfs
-        .dht_get("<ipns key here>")
-        .await?;
+    let mut stream = ipfs.dht_get(&opt.key).await?;
 
     while let Some(record) = stream.next().await {
         //TODO: Look into a better conversion
         let key = &record.key.as_ref()[6..];
         let encoded_key = &multibase::encode(Base::Base58Btc, key)[1..];
         println!("Record Key: {encoded_key}");
+        println!("Publisher: {:?}", record.publisher);
+        println!("Expires: {:?}", record.expires);
         println!();
     }
 
