8a4744a444ca6565764d68700ad9f292722854da
Darius Clark <darius.clark@proton.me>
2023-11-17 22:44:34 -0500

    chore: Update dag examples

--- examples/dag_creation.rs
+++ examples/dag_creation.rs
@@ -17,21 +17,19 @@ async fn main() -> anyhow::Result<()> {
         .await?;
 
     // Create a DAG
-    let f1 = ipfs.put_dag(ipld!("block1")).into_future();
-    let f2 = ipfs.put_dag(ipld!("block2")).into_future();
-    let (res1, res2) = join!(f1, f2);
-    let root = ipld!([res1?, res2?]);
+    let cid1: libipld::cid::CidGeneric<64> = ipfs.put_dag(ipld!("block1")).await?;
+    let cid2 = ipfs.put_dag(ipld!("block2")).await?;
+    let root = ipld!([cid1, cid2]);
     let cid = ipfs.put_dag(root).await?;
     let path = IpfsPath::from(cid);
 
     // Query the DAG
     let path1 = path.sub_path("0")?;
     let path2 = path.sub_path("1")?;
-    let f1 = ipfs.get_dag(path1).into_future();
-    let f2 = ipfs.get_dag(path2).into_future();
-    let (res1, res2) = join!(f1, f2);
-    println!("Received block with contents: {:?}", res1?);
-    println!("Received block with contents: {:?}", res2?);
+    let block1 = ipfs.get_dag(path1).await?;
+    let block2 = ipfs.get_dag(path2).await?;
+    println!("Received block with contents: {:?}", block1);
+    println!("Received block with contents: {:?}", block2);
 
     // Exit
     ipfs.exit_daemon().await;
--- examples/repo_dag.rs
+++ examples/repo_dag.rs
@@ -14,22 +14,19 @@ async fn main() -> anyhow::Result<()> {
     let repo = Repo::new_memory();
     let dag = IpldDag::from(repo.clone());
 
-    // Create a DAG
-    let f1 = dag.put_dag(ipld!("block1")).into_future();
-    let f2 = dag.put_dag(ipld!("block2")).into_future();
-    let (res1, res2) = join!(f1, f2);
-    let root = ipld!([res1?, res2?]);
+    let cid1: libipld::cid::CidGeneric<64> = dag.put_dag(ipld!("block1")).await?;
+    let cid2 = dag.put_dag(ipld!("block2")).await?;
+    let root = ipld!([cid1, cid2]);
     let cid = dag.put_dag(root).await?;
     let path = IpfsPath::from(cid);
 
     // Query the DAG
     let path1 = path.sub_path("0")?;
     let path2 = path.sub_path("1")?;
-    let f1 = dag.get_dag(path1).into_future();
-    let f2 = dag.get_dag(path2).into_future();
-    let (res1, res2) = join!(f1, f2);
-    println!("Received block with contents: {:?}", res1?);
-    println!("Received block with contents: {:?}", res2?);
+    let block1 = dag.get_dag(path1).await?;
+    let block2 = dag.get_dag(path2).await?;
+    println!("Received block with contents: {:?}", block1);
+    println!("Received block with contents: {:?}", block2);
 
     repo.shutdown();
     Ok(())
