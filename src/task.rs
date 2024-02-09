03b87b69e1ff7239c8503227479957e084c8e08e
Darius Clark <darius.clark@proton.me>
2024-02-09 09:57:35 -0500

    chore: Change order of storing and broadcasting identify info

--- src/task.rs
+++ src/task.rs
@@ -888,26 +888,15 @@ impl<C: NetworkBehaviour<ToSwarm = void::Void>> IpfsTask<C> {
 
             SwarmEvent::Behaviour(BehaviourEvent::Identify(event)) => match event {
                 IdentifyEvent::Received { peer_id, info } => {
-                    self.swarm
-                        .behaviour_mut()
-                        .peerbook
-                        .inject_peer_info(info.clone());
-
-                    if let Some(rets) = self.dht_peer_lookup.remove(&peer_id) {
-                        for ret in rets {
-                            let _ = ret.send(Ok(info.clone()));
-                        }
-                    }
-
                     let IdentifyInfo {
                         listen_addrs,
                         protocols,
                         ..
-                    } = info;
+                    } = &info;
 
                     if let Some(kad) = self.swarm.behaviour_mut().kademlia.as_mut() {
                         if protocols.iter().any(|p| libp2p::kad::PROTOCOL_NAME.eq(p)) {
-                            for addr in &listen_addrs {
+                            for addr in listen_addrs {
                                 kad.add_address(&peer_id, addr.clone());
                             }
                         }
@@ -919,10 +908,18 @@ impl<C: NetworkBehaviour<ToSwarm = void::Void>> IpfsTask<C> {
                     {
                         if let Some(autonat) = self.swarm.behaviour_mut().autonat.as_mut() {
                             for addr in listen_addrs {
-                                autonat.add_server(peer_id, Some(addr));
+                                autonat.add_server(peer_id, Some(addr.clone()));
                             }
                         }
                     }
+
+                    if let Some(rets) = self.dht_peer_lookup.remove(&peer_id) {
+                        for ret in rets {
+                            let _ = ret.send(Ok(info.clone()));
+                        }
+                    }
+
+                    self.swarm.behaviour_mut().peerbook.inject_peer_info(info);
                 }
                 event => debug!("identify: {:?}", event),
             },
