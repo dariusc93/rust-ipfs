f839d1a5ccbcb050d281fc67da09f872dc42e61d
Darius Clark <darius.clark@proton.me>
2024-06-09 15:59:20 -0400

    chore: Update example

--- examples/wasm-example/src/lib.rs
+++ examples/wasm-example/src/lib.rs
@@ -33,7 +33,7 @@ pub async fn run() -> Result<(), JsError> {
     let addrs = node_a.listening_addresses().await.unwrap();
 
     for addr in addrs {
-        node_b.add_peer(peer_id, addr).await.unwrap();
+        node_b.add_peer((peer_id, addr)).await.unwrap();
     }
 
     node_b.connect(peer_id).await.unwrap();
