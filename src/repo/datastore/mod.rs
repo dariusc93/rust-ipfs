3bece1291c043d6b856590c04faca3e8c0667702
Darius Clark <dariusc93@users.noreply.github.com>
2024-09-07 00:20:28 -0400

    refactor: Remove sled and redb dependency and datastore usage (#304)

--- src/repo/datastore/mod.rs
+++ src/repo/datastore/mod.rs
@@ -1,12 +1,6 @@
 #[cfg(not(target_arch = "wasm32"))]
 pub mod flatfs;
 pub mod memory;
-#[cfg(not(target_arch = "wasm32"))]
-#[cfg(feature = "redb_data_store")]
-pub mod redb;
-#[cfg(not(target_arch = "wasm32"))]
-#[cfg(feature = "sled_data_store")]
-pub mod sled;
 
 #[cfg(target_arch = "wasm32")]
 pub mod idb;
