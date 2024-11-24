6db1a1ac04b6a03b81f01e3c0c93c9168225c035
Darius <darius.clark@proton.me>
2024-11-23 21:49:04 -0600

    chore: add example

---
+++ examples/request-response.rs
@@ -0,0 +1,56 @@
+use bytes::Bytes;
+use futures::StreamExt;
+
+use rust_ipfs::UninitializedIpfsDefault as UninitializedIpfs;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let node_a = UninitializedIpfs::new()
+        .with_default()
+        .add_listening_addr("/ip4/127.0.0.1/tcp/0".parse()?)
+        .with_request_response(Default::default())
+        .start()
+        .await?;
+
+    let node_b = UninitializedIpfs::new()
+        .with_default()
+        .add_listening_addr("/ip4/127.0.0.1/tcp/0".parse()?)
+        .with_request_response(Default::default())
+        .start()
+        .await?;
+
+    let peer_id = node_a.keypair().public().to_peer_id();
+    let peer_id_b = node_b.keypair().public().to_peer_id();
+
+    println!("Our Node (A): {peer_id}");
+    println!("Their Node (B): {peer_id_b}");
+
+    let addrs = node_a.listening_addresses().await?;
+
+    for addr in addrs {
+        node_b.add_peer((peer_id, addr)).await?;
+    }
+
+    let mut node_a_st = node_a.requests_subscribe().await?;
+
+    tokio::spawn(async move {
+        while let Some((pid, request, response)) = node_a_st.next().await {
+            let res_str = String::from_utf8_lossy(&request);
+            println!("{pid} requested {res_str} from {peer_id}");
+            let res = Bytes::copy_from_slice(b"pong");
+            let _ = response.send(res);
+            break;
+        }
+    });
+
+    let response = node_b.send_request(peer_id, b"ping").await?;
+
+    println!(
+        "{peer_id} responded with {}",
+        String::from_utf8_lossy(&response)
+    );
+
+    node_a.exit_daemon().await;
+    node_b.exit_daemon().await;
+    Ok(())
+}
