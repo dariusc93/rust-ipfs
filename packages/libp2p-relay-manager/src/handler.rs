311a002aa3fc59565c7f2eb17af3b1cd87618b03
Darius Clark <darius.clark@proton.me>
2025-03-06 13:31:17 -0500

    refactor: use CommunicationTask to handle sending message to IpfsTask.

--- packages/libp2p-relay-manager/src/handler.rs
+++ packages/libp2p-relay-manager/src/handler.rs
@@ -51,8 +51,7 @@ impl ConnectionHandler for Handler {
         false
     }
 
-    fn on_behaviour_event(&mut self, _event: Self::FromBehaviour) {
-    }
+    fn on_behaviour_event(&mut self, _event: Self::FromBehaviour) {}
 
     #[allow(clippy::wildcard_in_or_patterns)]
     fn on_connection_event(
