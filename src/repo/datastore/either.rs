13ac608baeed654322f87bc3e173988229bc4a48
Darius <darius.clark@proton.me>
2025-03-24 10:41:03 -0500

    chore: remove redundant ref keywords

--- src/repo/datastore/either.rs
+++ src/repo/datastore/either.rs
@@ -9,43 +9,43 @@ use crate::{PinKind, PinMode};
 impl<L: DataStore, R: DataStore> DataStore for Either<L, R> {
     async fn init(&self) -> Result<(), Error> {
         match self {
-            Either::Left(ref datastore) => datastore.init().await,
-            Either::Right(ref datastore) => datastore.init().await,
+            Either::Left(datastore) => datastore.init().await,
+            Either::Right(datastore) => datastore.init().await,
         }
     }
 
     async fn contains(&self, key: &[u8]) -> Result<bool, Error> {
         match self {
-            Either::Left(ref datastore) => datastore.contains(key).await,
-            Either::Right(ref datastore) => datastore.contains(key).await,
+            Either::Left(datastore) => datastore.contains(key).await,
+            Either::Right(datastore) => datastore.contains(key).await,
         }
     }
 
     async fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>, Error> {
         match self {
-            Either::Left(ref datastore) => datastore.get(key).await,
-            Either::Right(ref datastore) => datastore.get(key).await,
+            Either::Left(datastore) => datastore.get(key).await,
+            Either::Right(datastore) => datastore.get(key).await,
         }
     }
 
     async fn put(&self, key: &[u8], value: &[u8]) -> Result<(), Error> {
         match self {
-            Either::Left(ref datastore) => datastore.put(key, value).await,
-            Either::Right(ref datastore) => datastore.put(key, value).await,
+            Either::Left(datastore) => datastore.put(key, value).await,
+            Either::Right(datastore) => datastore.put(key, value).await,
         }
     }
 
     async fn remove(&self, key: &[u8]) -> Result<(), Error> {
         match self {
-            Either::Left(ref datastore) => datastore.remove(key).await,
-            Either::Right(ref datastore) => datastore.remove(key).await,
+            Either::Left(datastore) => datastore.remove(key).await,
+            Either::Right(datastore) => datastore.remove(key).await,
         }
     }
 
     async fn iter(&self) -> BoxStream<'static, (Vec<u8>, Vec<u8>)> {
         match self {
-            Either::Left(ref datastore) => datastore.iter().await,
-            Either::Right(ref datastore) => datastore.iter().await,
+            Either::Left(datastore) => datastore.iter().await,
+            Either::Right(datastore) => datastore.iter().await,
         }
     }
 }
@@ -53,15 +53,15 @@ impl<L: DataStore, R: DataStore> DataStore for Either<L, R> {
 impl<L: PinStore, R: PinStore> PinStore for Either<L, R> {
     async fn is_pinned(&self, block: &Cid) -> Result<bool, Error> {
         match self {
-            Either::Left(ref datastore) => datastore.is_pinned(block).await,
-            Either::Right(ref datastore) => datastore.is_pinned(block).await,
+            Either::Left(datastore) => datastore.is_pinned(block).await,
+            Either::Right(datastore) => datastore.is_pinned(block).await,
         }
     }
 
     async fn insert_direct_pin(&self, target: &Cid) -> Result<(), Error> {
         match self {
-            Either::Left(ref datastore) => datastore.insert_direct_pin(target).await,
-            Either::Right(ref datastore) => datastore.insert_direct_pin(target).await,
+            Either::Left(datastore) => datastore.insert_direct_pin(target).await,
+            Either::Right(datastore) => datastore.insert_direct_pin(target).await,
         }
     }
 
@@ -71,8 +71,8 @@ impl<L: PinStore, R: PinStore> PinStore for Either<L, R> {
         referenced: References<'_>,
     ) -> Result<(), Error> {
         match self {
-            Either::Left(ref datastore) => datastore.insert_recursive_pin(target, referenced).await,
-            Either::Right(ref datastore) => {
+            Either::Left(datastore) => datastore.insert_recursive_pin(target, referenced).await,
+            Either::Right(datastore) => {
                 datastore.insert_recursive_pin(target, referenced).await
             }
         }
@@ -80,8 +80,8 @@ impl<L: PinStore, R: PinStore> PinStore for Either<L, R> {
 
     async fn remove_direct_pin(&self, target: &Cid) -> Result<(), Error> {
         match self {
-            Either::Left(ref datastore) => datastore.remove_direct_pin(target).await,
-            Either::Right(ref datastore) => datastore.remove_direct_pin(target).await,
+            Either::Left(datastore) => datastore.remove_direct_pin(target).await,
+            Either::Right(datastore) => datastore.remove_direct_pin(target).await,
         }
     }
 
@@ -91,8 +91,8 @@ impl<L: PinStore, R: PinStore> PinStore for Either<L, R> {
         referenced: References<'_>,
     ) -> Result<(), Error> {
         match self {
-            Either::Left(ref datastore) => datastore.remove_recursive_pin(target, referenced).await,
-            Either::Right(ref datastore) => {
+            Either::Left(datastore) => datastore.remove_recursive_pin(target, referenced).await,
+            Either::Right(datastore) => {
                 datastore.remove_recursive_pin(target, referenced).await
             }
         }
@@ -103,8 +103,8 @@ impl<L: PinStore, R: PinStore> PinStore for Either<L, R> {
         mode: Option<PinMode>,
     ) -> BoxStream<'static, Result<(Cid, PinMode), Error>> {
         match self {
-            Either::Left(ref datastore) => datastore.list(mode).await,
-            Either::Right(ref datastore) => datastore.list(mode).await,
+            Either::Left(datastore) => datastore.list(mode).await,
+            Either::Right(datastore) => datastore.list(mode).await,
         }
     }
 
@@ -114,8 +114,8 @@ impl<L: PinStore, R: PinStore> PinStore for Either<L, R> {
         requirement: Option<PinMode>,
     ) -> Result<Vec<(Cid, PinKind<Cid>)>, Error> {
         match self {
-            Either::Left(ref datastore) => datastore.query(ids, requirement).await,
-            Either::Right(ref datastore) => datastore.query(ids, requirement).await,
+            Either::Left(datastore) => datastore.query(ids, requirement).await,
+            Either::Right(datastore) => datastore.query(ids, requirement).await,
         }
     }
 }
